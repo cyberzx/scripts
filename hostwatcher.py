379605297af868cecbc66670360e3dd2c934dd2a
cyberzx <cyberzx@gmail.com>
2012-07-22 02:06:55 +0400

    increased port range

--- hostwatcher.py
+++ hostwatcher.py
@@ -4,8 +4,6 @@ import json
 import struct
 import time
 
-# polling thread
-
 POLL_TIMEOUT = 5.0
 ONLINE_TIMEOUT = 30.0
 udpsock = None
@@ -86,7 +84,6 @@ def poll(hosts, port_range):
 init_socket()
 
 servers_list=["94.198.52.129", "94.198.52.130", "94.198.52.131"]
-#servers_list=["94.198.52.131"]
-poll(servers_list, range(5000, 5020))
+poll(servers_list, range(5000, 5050))
   
 
